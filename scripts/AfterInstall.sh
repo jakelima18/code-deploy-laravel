d77d5ff9afc4b404374d8069a14c328f663c1881
Jackson Lima <jake.lima18@gmail.com>
2020-01-08 08:08:29 -0300

    Corrigindo after install

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -3,12 +3,13 @@
 sudo chown -R www-data.www-data /var/www/html/storage
 sudo chown -R www-data.www-data /var/www/html/bootstrap/cache
 cd /var/www/html
-#
 # Run composer
+sudo composer install
 #configure .env - conexao com o banco
-#sudo mv .env.example .env
-#sudo sed -i 's/DB_DATABASE.*/DB_DATABASE=/g' .env
-#sudo sed -i 's/DB_HOST.*/DB_HOST=db-apirevenda.cijqqawmxwsg.us-east-1.rds.amazonaws.com/g' .env
-#sudo sed -i 's/DB_PASSWORD.*/DB_PASSWORD=/g' .env
+sudo mv .env.example .env
+sudo sed -i 's/DB_DATABASE.*/DB_DATABASE=forum/g' .env
+sudo sed -i 's/DB_USERNAME.*/DB_USERNAME=son/g' .env
+sudo sed -i 's/DB_PASSWORD.*/DB_PASSWORD=schoolofnet/g' .env
 # Run artisan commands
+sudo php /var/www/html artisan key:generate
 sudo php /var/www/html/artisan migrate
