eef7df9bdecc7fc333ff4edd984b63307839e855
Jackson Lima <jake.lima18@gmail.com>
2020-01-08 08:11:16 -0300

    Corrigindo after install

--- scripts/AfterInstall.sh
+++ scripts/AfterInstall.sh
@@ -11,5 +11,5 @@ sudo sed -i 's/DB_DATABASE.*/DB_DATABASE=forum/g' .env
 sudo sed -i 's/DB_USERNAME.*/DB_USERNAME=son/g' .env
 sudo sed -i 's/DB_PASSWORD.*/DB_PASSWORD=schoolofnet/g' .env
 # Run artisan commands
-sudo php /var/www/html artisan key:generate
+sudo php /var/www/html/artisan key:generate
 sudo php /var/www/html/artisan migrate
